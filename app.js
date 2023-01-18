03e4d234dfe07aad1060321c4eaccdb1a086f13a
quantum <partnerprods@yandex.ru>
2023-01-19 03:01:44 +0700

    fix: исправить раздачу статики

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+
 require('dotenv').config();
 
 const app = express();
 
-app.use('/', express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist`));
+
+app.use('/*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
 
 module.exports = app;
